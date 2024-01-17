ba0ab8a8fcf72f8209545c608e42913d4f2da55b
Marek Baran ntb <baran.m1505@gmail.com>
2024-01-17 21:02:59 +0100

    working

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 
 def get_pdf_text(pdf_docs):
